1f7293b4f45ee8c9da723c9f88928c9c23015193
mostafazhrn <mostafazhrn@gmail.com>
2024-07-03 11:39:50 -0400

    push_task1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,23 @@ import logging
 import os
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+    REDACTION = '***'
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ';'
+
+    def __init__(self, fields: List[str]):
+        """ this shall rep constructor  method"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ This shall rep the format method"""
+        res = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, res, self.SEPARATOR)
+
+
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str, separator: str) -> str:
