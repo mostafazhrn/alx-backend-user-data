8c15038d26a190856689b7e15a865953979f9f54
mostafazhrn <mostafazhrn@gmail.com>
2024-07-04 11:28:37 -0400

    push_task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ This function shall hash a password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ This function shall validate a password"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
