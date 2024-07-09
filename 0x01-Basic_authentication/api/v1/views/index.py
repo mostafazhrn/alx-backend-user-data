e421b946a54f5cd12184b57f5502fc94d69ab796
mostafazhrn <mostafazhrn@gmail.com>
2024-07-09 07:07:49 -0400

    push_task1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ This instance shall handle unauthorized error handler """
+    return abort(401)
