1beb4cb4d32399aa4692a9a0756ba1733e36e006
mostafazhrn <mostafazhrn@gmail.com>
2024-07-09 08:07:36 -0400

    push_task3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" this script shall rep module for API status codes """
+from flask import Flask, jsonify, request, abort
+from typing import List, TypeVar
+
+
+class Auth:
+    """ this class shall rep the Auth class"""
+
+    def __init__(self):
+        """ this method shall initialize the Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ this method shall require authentication"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for pths in excluded_paths:
+            if pths.endswith('*'):
+                if path.startswith(pths[:-1]):
+                    return False
+            elif path == pths:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ this method shall return the authorization header"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ this method shall return the current user"""
+        return request
