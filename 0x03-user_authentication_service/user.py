6fa6760a81f078d28f055d82a3514d1d68bccb29
mostafazhrn <mostafazhrn@gmail.com>
2024-07-15 16:03:50 -0400

    push_task0_fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     """ THis instance shall strat the class User """
-    ___tablename__ = 'users'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
